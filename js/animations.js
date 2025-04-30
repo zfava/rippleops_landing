4a876719dec57f37cc58e61603778b143fe329fa
zfava <zack.fava@rippleops.ai>
2025-04-30 09:33:44 -0600

    Add files via upload

---
+++ js/animations.js
@@ -0,0 +1,13 @@
+const features = document.querySelectorAll('.animate-up');
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = 'translateY(0)';
+    }
+  });
+}, { threshold: 0.2 });
+
+features.forEach(feature => {
+  observer.observe(feature);
+});
